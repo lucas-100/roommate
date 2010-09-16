16839faa3c3031c4a677b9ed95095046cbe9f265
Jared McFarland <jaredmcfarland@Jared-McFarlands-iMac-2.local>
2010-09-15 18:07:25 -0700

    fixing send email bug

--- app/models/person.rb
+++ app/models/person.rb
@@ -23,8 +23,6 @@ class Person < ActiveRecord::Base
                     :format     => { :with => Authentication.email_regex, :message => Authentication.bad_email_message },
                     :length     => { :within => 6..100 }
 
-  before_save :send_email
-
   # HACK HACK HACK -- how to do attr_accessible from here?
   # prevents a user from submitting a crafted form that bypasses activation
   # anything else you want your user to change should be added here.
