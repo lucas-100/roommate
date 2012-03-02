e548fecdd1aadd6983b84f1bc08c8712be3a3c5a
Jared McFarland <jared.online@gmail.com>
2012-03-01 22:58:01 -0800

    works?

--- public/assets/application-4df4b8e2b6abdef4240437c0bfa60dfd.js
+++
@@ -1,3 +0,0 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
-$("#houseEditLink").click(function(a){return $("#house_name_home").toggle(),$("#house_name").toggle(),!1});
